76492afd2763297a7cce95111d03177e89f004cd
Jun Qi Choo <junqi.cjq@gmail.com>
2025-10-04 21:26:36 +0800

    added controllers and database connection file

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const app = express()
+const app = express();
 const port = 3000
+app.use(cors());
+app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
